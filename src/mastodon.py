673ef1ef8964930388dd52f5f5f0f75f5f08ddc3
Aleksey R <eretik.box@gmail.com>
2022-08-04 18:29:30 +0300

    fix(mastodon): mastodon.publish

--- src/mastodon.py
+++ src/mastodon.py
@@ -19,5 +19,6 @@ def post_media(session, media_url):
 
 def publish(session, content, media):
     media_ids = [post_media(session, url) for url in media]
-    r = session.do('post', 'api/v1/statuses', data=dict(status=content, media_ids=media_ids))
+    r = session.do('post', 'api/v1/statuses',
+                   data=dict(status=content, content_type='text/html', media_ids=media_ids))
     return r['id']
